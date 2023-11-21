64c2db3ffd2917edc68ad318d4ac16a6b0ebccd9
Raul Vincent Rodriguez <124639153+Lockheer@users.noreply.github.com>
2023-11-21 16:05:50 +0800

    Add project files.

---
+++ LibrarySystem-MVC/Areas/Identity/Data/AppUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace LibrarySystem_MVC.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AppUser class
+public class AppUser : IdentityUser
+{
+
+    [Required(ErrorMessage = "Please enter your first name.")]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please enter your last name.")]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = string.Empty;
+
+
+
+}
+
---
+++ LibrarySystem-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibrarySystem_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
